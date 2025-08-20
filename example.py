bd81cdcc49e72f30c13eb8e76e3be0979ddfa7a4
wilmix <wmx.seo@gmail.com>
2025-08-20 09:06:05 -0400

    feat: update invoice processing configuration with new values and path

--- example.py
+++ example.py
@@ -2,17 +2,16 @@ from src.xml_to_sql import procesar_e_insertar_factura
 
 if __name__ == "__main__":
     # --- Configuración de la Factura a Procesar ---
-    cuf_factura = "447D970043366B0939995E2E850A1F29B11E401AC462ED618D52D1F74"
-    id_factura_db = 108799
-    pedido_opcional = ""
-    numero_factura = "62"
-    total_factura = 117.97
+    cuf_factura = "447D97004336937AFD837DCA282A6E49EC3E55778163225E024302F74"
+    id_factura_db = 110086
+    pedido_opcional = "PO 125806"
+    numero_factura = "76"
+    total_factura =  2910.67
 
-
-    
+   
     # --- Configuración de Rutas ---
     # Ajusta las rutas según tu estructura de carpetas C:/Users/willy/OneDrive/Desktop/obsFacturas/siat_xml_to_sql/data
-    ruta_base_xml = "C:/Users/willy/OneDrive/Desktop/obsFacturas/siat_xml_to_sql/data"  # Ajusta esta ruta según tu estructura de carpetas XML
+    ruta_base_xml = r"C:\Users\willy\OneDrive\Documentos\xml\2025"  # Ajusta esta ruta según tu estructura de carpetas XML
     archivo_config_db = "db_config.ini" # Ruta al archivo de configuración de la BD
 
     # --- Procesar e Intentar Insertar Factura ---
